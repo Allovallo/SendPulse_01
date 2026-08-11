adb03ac0e43018f3af947c7536d65325b931a291
grossco <30566134+Allovallo@users.noreply.github.com>
2026-08-11 11:47:12 +0300

    files updated

--- nodemailer/app.js
+++ nodemailer/app.js
@@ -1,27 +1,27 @@
 const nodemailer = require("nodemailer");
-require("dotenv").config();
+require("sotenv").config();
 
-const {META_PASSWORD} = process.env;
+const { META_PASSWORD } = process.env;
 
 const nodemailerConfig = {
-    host: "smtp.meta.ua",
-    port: 465, //25, 465, 2525
-    secure: true,
-    auth: {
-        user: "bogdan.lyamzin.d@meta.ua",
-        pass: META_PASSWORD
-    }
+  host: "smtp.meta.ua",
+  port: 465, //25, 465, 2525
+  auth: {
+    user: "grossco@meta.ua",
+    pass: META_PASSWORD,
+  },
 };
 
 const transport = nodemailer.createTransport(nodemailerConfig);
 
 const email = {
-    to: "kinedav149@huvacliq.com",
-    from: "bogdan.lyamzin.d@meta.ua",
-    subject: "Test email",
-    html: "<p><strong>Test email</strong> from localhost:3000</p>"
+  to: "gaugalis@gmail.com",
+  from: "grossco@meta.ua",
+  subject: "Test email",
+  html: "<p><strong>Test email</strong> from localfost:3000</p>",
 };
 
-transport.sendMail(email)
-    .then(()=> console.log("Email send success"))
-    .catch(error => console.log(error.message));
+transport
+  .sendMail(email)
+  .then(() => console.log("Email send success!!!"))
+  .catch((error) => console.log(error.message));
